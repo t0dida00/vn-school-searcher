4c998344525ebde89b712351dda221cee56bae6e
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2025-06-20 01:06:17 +0300

    update baseUrl

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -6,8 +6,11 @@ export const revalidate = 60; // ISR or 0 for pure SSG
 export default async function HomePage() {
     let data = null;
     let error = null;
+    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+    const host = process.env.VERCEL_URL || 'localhost:3000';
+    const baseUrl = `${protocol}://${host}`;
     try {
-        const res = await axios.get('http://localhost:3000/api/universities');
+        const res = await axios.get(`${baseUrl}/api/universities`);
         data = res.data;
     } catch (err: any) {
         if (axios.isAxiosError(err)) {
