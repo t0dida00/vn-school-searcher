19ae4541db4634c699ba7e72c9991a0c1fc6cb6d
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2025-06-20 03:53:42 +0300

    test

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -6,18 +6,17 @@ export const revalidate = 60; // ISR or 0 for pure SSG
 export default async function HomePage() {
     let data = null;
     let error = null;
-
-    const host = process.env.VERCEL_URL || 'http://localhost:3000';
+    const baseUrl = process.env.NEXT_INTERNAL_BASE_URL || 'http://localhost:3000';
 
     try {
-        const res = await fetch(`${host}/api/universities`);
+        const res = await fetch(`${baseUrl}/api/universities`);
         data = await res.json();
     } catch (err: any) {
         if (axios.isAxiosError(err)) {
-            console.log(err)
+            console.log(err, 'Axios error');
             error = err.response?.data?.message || err.message || 'Axios error occurred';
         } else {
-            console.log(error)
+            console.log(err, 'Unexpected error');
             error = 'An unexpected error occurred';
         }
     }
