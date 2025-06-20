0b2cf6a9418444e4134a779bd20bd42d8809a4ed
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2025-06-20 22:58:19 +0300

    update font-size how to don't zoom in the search

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -53,6 +53,7 @@ export default function Search({
                     onChange={(e) => setQuery?.(e.target.value)}
                     className={cn(
                         "pl-12 pr-4 py-4",
+                        "text-base",
                         "rounded-full bg-white text-sm",
                         "shadow-[0_2px_6px_rgba(0,0,0,0.3)]",
                         "border-none outline-none",
